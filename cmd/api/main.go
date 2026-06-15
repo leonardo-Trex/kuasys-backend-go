7c812bbfee9f5e46014d9912999f8e630239b745
Leonardo <leonardosoares@unitins.br>
2026-06-15 16:30:36 -0300

    feat(setup): install go-chi and configure base HTTP server
    - Add go-chi/chi/v5 library as the routing dependency.
    
    - Configure the HTTP server on port 8080 inside cmd/api/main.go.
    
    - Enable default Logger and Recoverer middlewares.
    
    - Create initial '/' endpoint responding with 'Hello World com Go-Chi!'.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World com Go-Chi!"))
+	})
+
+	port := ":8080"
+	fmt.Printf("Servidor rodando com sucesso na porta %s...\n", port)
+
+	err := http.ListenAndServe(port, r)
+	if err != nil {
+		fmt.Printf("Erro ao iniciar o servidor: %v\n", err)
+	}
 }
